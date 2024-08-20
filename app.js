00ab527bd699d5585f1271d08afced1eaa915bb9
marcionevesgoncalves <mnevesg@yahoo.com.br>
2024-08-20 07:46:12 -0300

    Commit para acionar o GitHub Action

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express();
 
 const PORT = 3001
-let msn = "Hello World - Part master 2!"
+let msn = "Hello World - Part final!"
 
 app.get('/',(req,res)=>{
     res.send(msn)
