76d309d21a1e6098142007c9555e923e86c94694
Marcio Neves <57576940+marcionevesgoncalves@users.noreply.github.com>
2024-08-20 05:51:00 -0300

    Update app.js
    Inclui Part master 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express();
 
 const PORT = 3001
-let msn = "Hello World - Part master!"
+let msn = "Hello World - Part master 2!"
 
 app.get('/',(req,res)=>{
     res.send(msn)
